3fd544b27533653f9a07ef43425c72b76c71c14e
Bavoez <nasrulllatief@gmail.com>
2023-07-06 12:42:22 +0700

    UASweb

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -48,10 +48,12 @@ class Welcome extends CI_Controller
     {
         $user_id = $this->input->post('username');
         $artikel = $this->input->post('artikel');
+        $jenis = $this->input->post('jenis');
 
         $post = new Post();
         $post->user_id = $user_id ;
-        $post->artikel = $artikel;
+        $post->article = $article;
+        $post->jenis = $jenis;
         $post->save();
 
         redirect ('Welcome/tampil');
@@ -67,16 +69,23 @@ class Welcome extends CI_Controller
 
     public function ubah($id)
     {
+        
         $avail_user = User::all();
         $post = Post::find($id);
-        $this->_createView('update', ['post' => $post, 'avail_user'=> $avail_user]);
+        $jenis = 0;
+        if($post->jenis == 'Berita') $jenis = 0;
+        else if($post->jenis == 'Tutorial') $jenis = 1;
+        else if($post->jenis == 'Blog') $jenis = 2;
+
+        $this->_createView('update', ['post' => $post, 'avail_user' => $avail_user, 'jenis' => $jenis]);
     }
 
     public function update($id)
     {
         $post = Post::find($id);
         $post->user_id = $this->input->post('username');
-        $post->artikel = $this->input->post('artikel');
+        $post->article = $this->input->post('article');
+        $post->jenis = $this->input->post('jenis');
         $post->save();
 
         redirect ('Welcome/tampil');
